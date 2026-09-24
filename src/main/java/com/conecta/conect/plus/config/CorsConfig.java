ffc5d42ceba52814e5197974c3a6032dc841c718
Leonardo Zaize <zaizeleonardo@gmail.com>
2026-09-24 14:49:09 -0300

    permitir frontend da Vercel no CORS

--- src/main/java/com/conecta/conect/plus/config/CorsConfig.java
+++ src/main/java/com/conecta/conect/plus/config/CorsConfig.java
@@ -11,7 +11,10 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+        .allowedOrigins(
+        	    "http://localhost:5173",
+        	    "https://conecta-plus-front-45xvqfg0s-zaize.vercel.app"
+        	)
                 .allowedMethods(
                         "GET",
                         "POST",
